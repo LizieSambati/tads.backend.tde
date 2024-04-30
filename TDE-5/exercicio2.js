503ebdc1a056a86bc1bb66fa7027e02a6a271944
LizieSambati <159593262+LizieSambati@users.noreply.github.com>
2024-04-30 20:51:25 -0300

    Add files via upload

---
+++ TDE-5/exercicio2.js
@@ -0,0 +1,18 @@
+// 2. usando a lib fakerjs (https://www.npmjs.com/package/@faker-js/faker) 
+// crie uma função que retorna dados de uma pessoa falsa contendo nome, email e data de nascimento.
+
+// ESM
+import { faker } from '@faker-js/faker';
+
+// CJS
+const { faker } = require('@faker-js/faker');
+
+function createRandomUser() {
+  return {
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    birthdate: faker.date.birthdate(),
+  };
+}
+
+module.exports = {createRandomUser}
